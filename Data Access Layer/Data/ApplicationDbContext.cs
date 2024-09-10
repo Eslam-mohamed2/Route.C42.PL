f17ffc091acf631f0b3ec6a257e20f90cd7d2bae
Eslam mohamed <em354219@gmail.com>
2024-09-10 22:56:31 +0300

    DAL for Department Module

---
+++ Data Access Layer/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Data_Access_Layer.Data.Configrutions;
+using Data_Access_Layer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server = .; Database = MVCApplication; Trusted_Connection = True; MultipleActiveResultSets = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigrutions());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        DbSet<Department> Departments { get; set; }
+    }
+}
